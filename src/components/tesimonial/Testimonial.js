c91162eea6e2842d87319653da30ce7f67913dca
Kamranislam0123 <Kamranislam8@gmail.com>
2024-10-06 17:03:55 +0600

    testimonial updated

--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import { RiStarFill } from "react-icons/ri";
 import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
 import Title from '../layouts/Title'
-import { testimonialOne,testimonialTwo, quote } from "../../assets";
+// import { testimonialOne,testimonialTwo, quote } from "../../assets";
 
 
 function SampleNextArrow(props) {
@@ -96,39 +96,24 @@ const Testimonial = () => {
         <Title title="WHAT CLIENTS SAY" des="Testimonial" />
       </div>
       <div className="max-w-6xl mx-auto">
-        {/* ================ Slider One ================== */}
         <Slider {...settings}>
+          {/* ================ Slider One ================== */}
           <div className="w-full">
-            <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
-                  className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                  src={testimonialOne}
-                  alt="testimonialOne"
-                />
-                <div className="w-full flex flex-col justify-end">
-                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Freelance
-                  </p>
-                  <h3 className="text-2xl font-bold">Seeam Ahmod</h3>
-                  <p className="text-base tracking-wide text-gray-500">
-                    UI/UX Designer
-                  </p>
-                </div>
-              </div>
-              <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
-                <img className="w-20 lgl:w-32" src={quote} alt="quote" />
-                <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
+            <div className="w-full h-auto flex flex-col justify-between">
+              <div className="w-full h-full flex flex-col justify-between">
+                <div className="w-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      <h3 className="text-2xl font-bold text-designColor">Seeam Ahmod</h3>
+                      <p className="text-base text-gray-400 mt-2">UI/UX Designer</p>
+                      <h4 className="text-xl lgl:text-2xl font-medium tracking-wide mt-4">
                         Website Redesign Project
-                      </h3>
+                      </h4>
                       <p className="text-base text-gray-400 mt-3">
                         via Upwork - Jan 15, 2023 - Mar 22, 2023
                       </p>
                     </div>
-                    <div className="text-yellow-500 flex gap-1">
+                    <div className="text-yellow-500 flex gap-1 mt-4">
                       <RiStarFill />
                       <RiStarFill />
                       <RiStarFill />
@@ -145,36 +130,21 @@ const Testimonial = () => {
           </div>
           {/* ================ Slider Two ================== */}
           <div className="w-full">
-            <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
-                  className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                  src={testimonialTwo}
-                  alt="testimonialTwo"
-                />
-                <div className="w-full flex flex-col justify-end">
-                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
-                  </p>
-                  <h3 className="text-2xl font-bold">Mizanur Rahman</h3>
-                  <p className="text-base tracking-wide text-gray-500">
-                    Software Engineer
-                  </p>
-                </div>
-              </div>
-              <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
-                <img className="w-20 lgl:w-32" src={quote} alt="quote" />
-                <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
+            <div className="w-full h-auto flex flex-col justify-between">
+              <div className="w-full h-full flex flex-col justify-between">
+                <div className="w-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      <h3 className="text-2xl font-bold text-designColor">Mizanur Rahman</h3>
+                      <p className="text-base text-gray-400 mt-2">Software Engineer</p>
+                      <h4 className="text-xl lgl:text-2xl font-medium tracking-wide mt-4">
                         E-commerce Platform Development
-                      </h3>
+                      </h4>
                       <p className="text-base text-gray-400 mt-3">
                         via LinkedIn - Apr 10, 2023 - Jul 5, 2023
                       </p>
                     </div>
-                    <div className="text-yellow-500 flex gap-1">
+                    <div className="text-yellow-500 flex gap-1 mt-4">
                       <RiStarFill />
                       <RiStarFill />
                       <RiStarFill />
@@ -191,36 +161,21 @@ const Testimonial = () => {
           </div>
           {/* ================ Slider Three ================== */}
           <div className="w-full">
-            <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
-                  className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                  src={testimonialOne}
-                  alt="testimonialOne"
-                />
-                <div className="w-full flex flex-col justify-end">
-                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Freelance
-                  </p>
-                  <h3 className="text-2xl font-bold">Novojit Das</h3>
-                  <p className="text-base tracking-wide text-gray-500">
-                    Software Engineer
-                  </p>
-                </div>
-              </div>
-              <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
-                <img className="w-20 lgl:w-32" src={quote} alt="quote" />
-                <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
+            <div className="w-full h-auto flex flex-col justify-between">
+              <div className="w-full h-full flex flex-col justify-between">
+                <div className="w-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      <h3 className="text-2xl font-bold text-designColor">Novojit Das</h3>
+                      <p className="text-base text-gray-400 mt-2">Software Engineer</p>
+                      <h4 className="text-xl lgl:text-2xl font-medium tracking-wide mt-4">
                         Mobile App Development
-                      </h3>
+                      </h4>
                       <p className="text-base text-gray-400 mt-3">
                         via GitHub - Sep 1, 2023 - Nov 30, 2023
                       </p>
                     </div>
-                    <div className="text-yellow-500 flex gap-1">
+                    <div className="text-yellow-500 flex gap-1 mt-4">
                       <RiStarFill />
                       <RiStarFill />
                       <RiStarFill />
