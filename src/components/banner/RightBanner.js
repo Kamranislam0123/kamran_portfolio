be86e5c971de8b0fdcff8489936b3708b710386f
Kamranislam0123 <Kamranislam8@gmail.com>
2024-11-29 17:27:25 +0600

    image fixed

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -3,13 +3,12 @@ import { profile } from "../../assets/index";
 
 const RightBanner = () => {
   return (
-    <div className="w-full lgl:w-1/2 hidden md:flex justify-center items-center relative ">
+    <div className="w-full lgl:w-1/2 hidden md:flex justify-center items-center relative">
       <img
-        className="w-full h-auto pl-5 transform scale-125 transition-transform duration-300 hover:scale-135"
+        className="w-auto max-w-none h-auto object-contain transform scale-175 transition-transform duration-300 hover:scale-190"
         src={profile}
         alt="bannerImg"
       />
-      {/* <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div> */}
     </div>
   );
 };
