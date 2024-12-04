b7b4fd1efad94ce120d5f7c1811ad48f93ecfe2d
Kamranislam0123 <Kamranislam8@gmail.com>
2024-12-04 14:37:16 +0600

    image changed

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -3,13 +3,15 @@ import { profile } from "../../assets/index";
 
 const RightBanner = () => {
   return (
-    <div className="w-full lgl:w-1/2 hidden md:flex justify-center items-center relative">
+    
+      <div className="w-full lgl:w-1/2 hidden md:flex justify-center items-center relative">
       <img
         className="w-auto max-w-none h-auto object-contain transform scale-175 transition-transform duration-300 hover:scale-190"
         src={profile}
         alt="bannerImg"
       />
     </div>
+    
   );
 };
 
